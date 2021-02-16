99b14761138693e11e526788fc4cc6e38d6db6ed
SerpentSpirale <serpentspirale@gmail.com>
2021-02-16 21:15:51 +0100

    - Added DisplayUtils files

---
+++ DisplayUtils.cpp
@@ -0,0 +1,19 @@
+#include "DisplayUtils.h"
+#include <iostream>
+
+using namespace std;
+
+void ClearDisplay(){
+    cout << "\033[H\033[2J";
+}
+
+void DisplayGame(GameMap & Map){
+    ClearDisplay();
+    //Display in a way to match the number pad.
+    for(int i(2); i>-1; --i){
+        for(unsigned j(0); j<3; ++j){
+            cout << string({Map[(i*3)+j],' '});
+        }
+        cout << endl;
+    }
+}
---
+++ DisplayUtils.h
@@ -0,0 +1,10 @@
+#ifndef DISPLAYUTILS_H
+#define DISPLAYUTILS_H
+
+#include "types.h"
+
+void ClearDisplay();
+
+void DisplayGame(GameMap & Map);
+
+#endif // DISPLAYUTILS_H
