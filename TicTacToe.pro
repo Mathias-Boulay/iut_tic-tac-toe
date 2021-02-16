8c9c089b1d4c5a9b70190aecdb687b39a6287ce0
SerpentSpirale <serpentspirale@gmail.com>
2021-02-16 21:16:40 +0100

    Created the MVP.

--- TicTacToe.pro
+++ TicTacToe.pro
@@ -5,10 +5,14 @@ CONFIG -= qt
 
 SOURCES += \
         AiUtils.cpp \
+        DisplayUtils.cpp \
+        GameUtils.cpp \
         InputUtils.cpp \
         main.cpp
 
 HEADERS += \
     AiUtils.h \
+    DisplayUtils.h \
+    GameUtils.h \
     InputUtils.h \
     types.h
--- main.cpp
+++ main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include "InputUtils.h"
+#include "AiUtils.h"
+#include "DisplayUtils.h"
+#include "GameUtils.h"
 
 using namespace std;
 
 int main()
 {
+    //TrainingData Data = GetTrainingData("file.txt");
+    srand(time(NULL));
+
+
+    AI AiInstance;
+    AiInstance._Token = Player2Token;
+    AiInstance._Data = GetTrainingData("file.txt");
+
+    GameMap GameMap;
+    for(unsigned i(0); i <9; ++i){
+        GameMap.push_back(' ');
+    }
+
+    while(true){
+        char input = ReadSpecificKeyboardInput({'1','2','3','4','5','6','7','8','9'});
+        GameMap[input-'1'] = Player1Token;
+
+        if(hasSomeoneWon(GameMap)){
+            TurnHistoryDataIntoTrainingData(AiInstance, false);
+            SaveTrainingData(AiInstance._Data, "file.txt");
+            break;
+        }
+
+        PlayTurn(GameMap, AiInstance);
+
+        if(hasSomeoneWon(GameMap)){
+            TurnHistoryDataIntoTrainingData(AiInstance, true);
+            SaveTrainingData(AiInstance._Data, "file.txt");
+            break;
+        }
+
+        DisplayGame(GameMap);
+
+    }
     cout << "Hello World!" << endl;
     return 0;
 }
