9de5b22fd8af17ed50b06af6d728bb0369ebbf24
SerpentSpirale <mathiasboulay@free.fr>
2021-06-13 13:07:01 +0200

    Add documentation for MenuUtils

--- MenuUtils.h
+++ MenuUtils.h
@@ -1,10 +1,33 @@
 #ifndef MENUUTILS_H
 #define MENUUTILS_H
 
+/*!
+ * \file MenuUtils.h
+ * \brief A set of functions to deal with menu interactions
+ * \author BOULAY Mathias
+ * \version 1.0
+ * \date 15 janvier 2021
+ */
+
+/**
+ * @brief Launch the main menu
+ * @fn void MainMenu();
+ * @return void
+ */
 void MainMenu();
 
+/**
+ * @brief Launch the AI menu
+ * @fn void AITrainingMenu();
+ * @return void
+ */
 void AITrainingMenu();
 
+/**
+ * @brief Display the tic tac toe header on the terminal
+ * @fn void DisplayHeader();
+ * @return void
+ */
 void DisplayHeader();
 
 #endif // MENUUTILS_H
